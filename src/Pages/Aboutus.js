f5a9ee18b37da417441fcce47650ba3da9ea772a
elrmam <ahmedredaelrmam@gmail.com>
2024-12-19 14:46:44 +0200

    updates

--- src/Pages/Aboutus.js
+++ src/Pages/Aboutus.js
@@ -4,7 +4,7 @@ function Aboutus(){
         <div className="container-fluid mb-5 " id='cources'>
             <div className="row text-center coursesbackgound mb-5">
                 <div className="col-12 my-5">
-                    <h1 className="yellow fw-bold">Who We Are ?</h1>
+                    <h1 className="yellow fw-bold">Who We Are</h1>
                     <br/>
                     <p className="fs-4 text-white darkblue">
                         Kernel Academy is a leading educational institution located in New Damietta.
@@ -16,8 +16,83 @@ function Aboutus(){
                     </p>
                 </div>
             </div>
-        </div>
-
+            <div className="row d-flex justify-content-center mx-5">
+                <div className="col-lg-4 col-md-6 col-sm-12 my-3 d-flex justify-content-center">
+                    <div className="row">
+                        <div className="col-12">
+                            <h1 className="darkblue">Our Mission</h1>
+                        </div>
+                        <div className="col-12">
+                            <h1 className="yellow mb-4">ــــــــــــــــــــــــــــــــــــــــــــ</h1>
+                        </div>
+                        <div className="col-12 ">
+                            <p className="fs-5 lightblue">
+                                Our mission is to create an engaging and supportive environment where students are encouraged to explore their passions, develop critical thinking skills, and achieve academic excellence. We believe in tailoring our teaching methods to suit each student's individual needs.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-lg-4 col-md-6 col-sm-12 my-3 d-flex justify-content-center">
+                    <div className="row">
+                        <div className="col-12">
+                            <h1 className="darkblue">Our Value</h1>
+                        </div>
+                        <div className="col-12">
+                            <h1 className="yellow mb-4">ــــــــــــــــــــــــــــــــــــــــــــ</h1>
+                        </div>
+                        <div className="col-12 ">
+                            <p className="fs-5 lightblue">
+                                At Kernel Academy, we are committed to fostering an environment of learning built on integrity, innovation, and inclusivity. Our values center on empowering students to reach their full potential by providing high-quality education, personalized support, and a culture of continuous growth.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-lg-4 col-md-6 col-sm-12 my-3 d-flex justify-content-center">
+                    <div className="row">
+                        <div className="col-12">
+                            <h1 className="darkblue">Our History</h1>
+                        </div>
+                        <div className="col-12">
+                            <h1 className="yellow mb-4">ــــــــــــــــــــــــــــــــــــــــــــ</h1>
+                        </div>
+                        <div className="col-12 ">
+                            <p className="fs-5 lightblue">
+                            Founded in 2024, Kernel Academy has quickly become a leading educational institution in the region. we’ve expanded our course offerings and are proud to help hundreds of students realize their academic and personal growth. Our commitment to excellence drives us to continually evolve and meet the needs of our students.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="row text-center mb-5 mx-5 mt-5">
+                <div className="row">
+                    <div className="col-12">
+                        <h1 className="darkblue fw-bold mt-5">Comfortable, Equipped Learning Space</h1>
+                        <br/>
+                        <div className="col-12">
+                            <h1 className="yellow mb-4">ـــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــ</h1>
+                        </div>
+                        <p className="fs-4 lighblue darkblue mb-5">
+                            At Kernel Academy, we prioritize your comfort and productivity. Our classrooms are equipped with comfortable chairs and practical desks to ensure maximum comfort. The air conditioning runs continuously to keep you cool and focused throughout your learning experience. We also provide projectors to display content clearly and effectively during lessons. The entire space is covered with Wi-Fi, ensuring seamless connectivity. Additionally, we offer laptops that are ready for use at any time .
+                        </p>
+                    </div>
+                    <div className="row mb-5">
+                        <div className="col-lg-3 col-md-6 col-sm-12">
+                            <img src="/academy1.jpg" className="rounded floating" alt="about" style={{ height: "30rem" }} />
+                        </div>
+                        <div className="col-lg-3 col-md-6 col-sm-12">
+                            <img src="/academy2.jpg" className="rounded floating" alt="about" style={{ height: "30rem" }} />
+                        </div>
+                        <div className="col-lg-3 col-md-6 col-sm-12">
+                            <img src="/academy3.jpg" className="rounded floating" alt="about" style={{ height: "30rem" }} />
+                        </div>
+                        <div className="col-lg-3 col-md-6 col-sm-12">
+                            <img src="/academy4.jpg" className="rounded floating" alt="about" style={{ height: "30rem" }} />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>  
+ 
     )
 }
 
